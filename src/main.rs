d1aa7c0adc1bb5485136fce2d0181b1080916539
loleik <lolei+github@lolei.uk>
2026-01-27 15:49:44 +0000

    Diffie-Hellman Key Agreement

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,5 @@ mod protocols;
 use protocols::dh::*;
 
 fn main() {
-    let params:Params = key_agreement(23, 5);
-
-    println!("{:?}", params);
+    key_agreement(23, 5);
 }
--- src/protocols/dh.rs
+++ src/protocols/dh.rs
@@ -5,8 +5,8 @@ pub struct Params {
     g: u32, // base
 
     // Secret integers
-    a: Option<u32>,
-    b: Option<u32>,
+    a: u32,
+    b: u32,
 
     // Public values
     pub_a: Option<u32>,
@@ -21,11 +21,42 @@ impl Params {
     fn init(p: u32, g: u32) -> Params {
         Params { 
             p: p, g: g, 
-            a: None, b: None, pub_a: None, pub_b: None, s_a: None, s_b: None 
+            a: 4, b: 3, 
+            pub_a: None, pub_b: None, s_a: None, s_b: None 
         }
     }
 }
 
-pub fn key_agreement(p: u32, g: u32) -> Params {
-    Params::init(p, g)
+fn public_values(params: &mut Params) -> &mut Params {
+    // A = (g ^ a) mod p
+    params.pub_a = Some( params.g.pow(params.a) % params.p );
+    // B = (g ^ b) mod p
+    params.pub_b = Some( params.g.pow(params.b) % params.p );
+
+    println!("pub_a / pub_b calculated: {:?}", params);
+
+    params
+}
+
+fn shared_secrets(params: &mut Params) -> &mut Params {
+    // s = (B ^ a) mod p
+    params.s_a = Some( params.pub_b.expect("None!").pow(params.a) % params.p );
+    // s = (A ^ b) mod p
+    params.s_b = Some( params.pub_a.expect("None!").pow(params.b) % params.p );
+
+    println!("s_a / s_b calculated: {:?}", params);
+
+    params
+}
+
+pub fn key_agreement(p: u32, g: u32) -> Result<(),()> {
+    let mut params: Params = Params::init(p, g);
+
+    println!("initialized: {:?}", params);
+
+    public_values(&mut params);
+
+    shared_secrets(&mut params);
+
+    Ok(())
 }
