13d730d7fee4bf2f2d7c8aade751ff76fa2bb306
loleik <lolei+github@lolei.uk>
2026-01-28 16:30:25 +0000

    Separated participants to better represent the protocol

--- src/protocols/dh.rs
+++ src/protocols/dh.rs
@@ -3,58 +3,69 @@ pub struct Params {
     // Public parameters
     p: u32, // modulus
     g: u32, // base
+}
 
-    // Secret integers
-    a: u32,
-    b: u32,
-
-    // Public values
-    pub_a: Option<u32>,
-    pub_b: Option<u32>,
-
-    // Shared secrets
-    s_a: Option<u32>,
-    s_b: Option<u32>,
+#[derive(Debug, Clone, Copy)]
+pub struct Participant {
+    secret: u32, // secret value
+    public: Option<u32>, // public value
+    shared: Option<u32>, // shared secret
 }
 
 impl Params {
     fn init(p: u32, g: u32) -> Params {
-        Params { 
-            p: p, g: g, 
-            a: 4, b: 3, 
-            pub_a: None, pub_b: None, s_a: None, s_b: None 
-        }
+        Params { p, g }
     }
 }
 
-fn public_values(params: &mut Params) -> &mut Params {
-    // A = (g ^ a) mod p
-    params.pub_a = Some( params.g.pow(params.a) % params.p );
-    // B = (g ^ b) mod p
-    params.pub_b = Some( params.g.pow(params.b) % params.p );
-
-    println!("pub_a / pub_b calculated: {:?}", params);
-
-    params
+impl Participant {
+    fn init(secret: u32) -> Participant {
+        Participant { secret, public: None, shared: None }
+    }
 }
 
-fn shared_secrets(params: &mut Params) -> &mut Params {
-    // s = (B ^ a) mod p
-    params.s_a = Some( params.pub_b.expect("None!").pow(params.a) % params.p );
-    // s = (A ^ b) mod p
-    params.s_b = Some( params.pub_a.expect("None!").pow(params.b) % params.p );
-
-    println!("s_a / s_b calculated: {:?}", params);
-
-    params
+fn public_value<'a>(
+    params: &'a Params, 
+    participant: &'a mut Participant
+) -> &'a mut Participant {
+    // public = (g ^ secret) mod p
+    participant.public = Some(
+        params.g.pow( participant.secret ) % params.p
+    );
+    
+    participant
 }
 
-pub fn key_agreement(p: u32, g: u32) {
-    let mut params: Params = Params::init(p, g);
+fn shared_secret<'a>(
+    params: &'a Params,
+    target: &'a mut Participant,
+    other: Participant
+) -> &'a mut Participant {
+    // s = (B ^ a) mod p and s = (A ^ b) mod p
+    target.shared = Some(
+        other.public.expect("None!").pow(target.secret) % params.p
+    );
+
+    target
+}
+
+pub fn key_agreement(
+    p: u32, 
+    g: u32
+) {
+    let params: Params = Params::init(p, g);
+    let mut alice: Participant = Participant::init(4);
+    let mut bob: Participant = Participant::init(3);
 
     println!("initialized: {:?}", params);
+    println!("Alice: {:?}", alice);
+    println!("Bob: {:?}", bob);
 
-    public_values(&mut params);
+    public_value(&params, &mut alice);
+    public_value(&params, &mut bob);
+    println!("Public values calculated: {:?} {:?}", alice, bob);
 
-    shared_secrets(&mut params);
+    shared_secret(&params, &mut alice, bob);
+    shared_secret(&params, &mut bob, alice);
+    println!("Shared secrets calculated: {:?} {:?}", alice, bob);
 }
